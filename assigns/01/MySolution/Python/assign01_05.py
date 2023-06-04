ab15bdec54a694c3add694ad7e86ff20d875390d
李易珈 <liyijia@liyijias-MacBook-Pro.local>
2023-06-03 20:21:19 -0400

    finish first question in q5

--- assigns/01/MySolution/Python/assign01_05.py
+++ assigns/01/MySolution/Python/assign01_05.py
@@ -12,3 +12,21 @@ print("[import ./../assign01_lib.py] is done!")
 # mylist_reverse (see list_reverse in assign01-lib.sml)
 #
 ####################################################
+def mylist_append(list1,list2):
+      if mylist_nilq(list1):
+        return list2
+      else return mylist_cons(list1.get_cons1(), mylist_append(list1.get_cons2(), list2))
+
+
+
+def mylist_rappend(list1,list2):
+    if len(list1) == 0:
+        return list2
+    else:
+        return mylist_rappend(list1[1:0],[list1[0]]+list2)
+    
+
+def mylist_reverse(list1):
+    return mylist_rappend(list1,[])
+
+
