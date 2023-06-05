d54f3c57a919223a7a6b17fdcb3bb072e13f4d5b
李易珈 <liyijia@liyijias-MacBook-Pro.local>
2023-06-05 12:17:10 -0400

    adding output.py which is a output file

---
+++ assigns/01/MySolution/Python/output.py
@@ -0,0 +1,46 @@
+"""
+output for 05
+
+e) liyijia@liyijias-MacBook-Pro Python % /usr/bin/python3 /Users/liyijia/cs320/cs320-hw1/assigns/01/Testing/Python/assign01_05_test.py
+ys = (1;2;3;4;5)
+[import ./../assign01_lib.py] is done!
+[import ./../../MySolution/Python/assign01_05.py] is done!
+xs = (1;2;3)
+xs = (1;2;3;1;2;3)
+xs = (3;2;1;3;2;1)
+xs = (1;2;3;1;2;3;3;2;1;3;2;1)
+Assign01-05-test passed!
+
+
+
+"""
+
+"""output for 06
+
+liyijia@liyijias-MacBook-Pro Python % /usr/bin/python3 /Users/liyijia/cs320/cs320-hw1/assigns/01/Testing/Python/assign01_06_test.py
+ys = (1;2;3;4;5)
+[import ./../assign01_lib.py] is done!
+[import ./../../assign01_lib.py] is done!
+[import ./../../MySolution/Python/assign01_06.py] is done!
+xs = (1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2)
+ys = (1;1;1;1;1;1;1;1;2;2;2;2;2;2;2;2;3;3;3;3;3;3;3;3;4;4;4;4;4;4;4;4)
+Assign01-06-test passed!
+
+
+
+
+"""
+
+""""
+output for 07
+
+liyijia@liyijias-MacBook-Pro Python % /usr/bin/python3 /Users/liyijia/cs320/cs320-hw1/assigns/01/Testing/Python/assign01_07_test.py
+ys = (1;2;3;4;5)
+[import ./../../assign01_lib.py] is done!
+[import ./../MySolution/assign01_07.py] is done!
+xs = (1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2;1;2;4;3;1;3;4;2)
+ys = (1;1;1;1;1;1;1;1;2;2;2;2;2;2;2;2;3;3;3;3;3;3;3;3;4;4;4;4;4;4;4;4)
+ys = (1;1;1;1;1;1;1;1;2;2;2;2;2;2;2;2;3;3;3;3;3;3;3;3;4;4;4;4;4;4;4;4)
+Assign01-07-test passed!
+
+"""
