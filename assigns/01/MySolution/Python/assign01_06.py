139a79bd05cf6cd9fb64aa94ad4ad400018462ab
李易珈 <liyijia@liyijias-MacBook-Pro.local>
2023-06-04 22:37:17 -0400

    I have no idea how to implement this without using appand

--- assigns/01/MySolution/Python/assign01_06.py
+++ assigns/01/MySolution/Python/assign01_06.py
@@ -10,3 +10,34 @@ print("[import ./../../assign01_lib.py] is done!")
 # mylist_quicksort (see list_quicksort in assign01-lib.sml)
 #
 ####################################################
+def mylist_quicksort(xs):
+    def qsort(xs):
+        if mylist_nilq(xs):
+            return mylist_nil()
+        else:
+            x1 = xs.get_cons1()
+            xs = xs.get_cons2()
+            ys,zs = qpart(xs,x1)
+            ys = qsort(ys)
+            zs = qsort(zs)
+
+            return mylist_append(ys,mylist_cons(x1,zs))
+        
+    def qpart(xs,p0):
+        if mylist_nilq(xs):
+            return mylist_nil(), mylist_nil()
+        else:
+            x1 = xs.get_cons1()
+            xs = xs.get_cons2()
+            ys,zs = qpart(xs,p0)
+            if x1 <= p0:
+                return mylist_cons(x1, ys), zs
+            else:
+                return ys, mylist_cons(x1, zs)
+    
+def mylist_append(list1,list2):
+      if mylist_nilq(list1):
+        return list2
+      else:
+        return mylist_cons(list1.get_cons1(), mylist_append(list1.get_cons2(), list2))
+
